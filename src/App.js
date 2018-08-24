45f0d2018e4989195e8ac9f06f8f3d07ff8f2611
sershishkov <ser.shishkov@gmail.com>
2018-08-24 11:16:35 +0300

    010-4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import AppNavbar from './components/layout/AppNavbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
+
 
 import './App.css';
 
@@ -11,7 +13,10 @@ class App extends Component {
         <div className="App">
           <AppNavbar />
           <div className="container">
-            <h1>sh-Hello</h1>          
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            
+            </Switch>        
           
           </div>
         </div>
