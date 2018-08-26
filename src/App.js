f723a5ca562bc6b64528d5accb46b092f6ffca64
sershishkov <ser.shishkov@gmail.com>
2018-08-26 11:30:19 +0300

    010-7

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import store from "./store";
 
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
+import AddClient from './components/clients/AddClient';
+
 
 
 import './App.css';
@@ -21,6 +23,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Dashboard} />
+                <Route exact path="/client/add" component={AddClient} />
               
               </Switch>        
             
--- src/store.js
+++ src/store.js
@@ -27,7 +27,11 @@ const rrfConfig = {
 //Init firebase instance
 firebase.initializeApp(firebaseConfig);
 //init firestore
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+
+
+  const settings = { timestampsInSnapshots: true};
+  firestore.settings(settings);
 
 // Add reactReduxFirebase enhancer when making store creator
 const createStoreWithFirebase = compose(
