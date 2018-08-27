ae459bc23d857788309657fd30f2b7e3657a8135
sershishkov <ser.shishkov@gmail.com>
2018-08-27 11:49:39 +0300

    010-10

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import store from "./store";
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
+
 
 
 import './App.css';
@@ -25,6 +27,8 @@ class App extends Component {
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
+
 
               
               </Switch>        
